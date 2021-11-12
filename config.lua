cab8bc90c7e5eb79a371e663f6e10002ebe4a1b8
CptNoHand <B_Marutschke@gmx.de>
2021-11-12 15:26:40 +0100

    mara umgesetzt

--- config.lua
+++ config.lua
@@ -6,6 +6,6 @@ Config.Gangs = {
     ['vagos'] = vector3(0, 0, 0),
     ['cartel'] = vector3(0, 0, 0),
     ['families'] = vector3(0, 0, 0),
-    ['marabunta'] = vector3(1241.89, -1576.52, 58.22),
+    ['marabunta'] = vector3(1281.26, -1619.63, 27.23),
     ['rooks'] = vector3(22.93, 6472.1, 31.9),
 }
