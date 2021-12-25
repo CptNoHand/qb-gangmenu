029905ff1e842e616c81847e0db16973e1e183a8
CptNoHand <90973107+CptNoHand@users.noreply.github.com>
2021-12-25 17:57:21 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,7 +6,7 @@ Config.Gangs = {
     ['vagos'] = vector3(0, 0, 0),
     ['cartel'] = vector3(0, 0, 0),
     ['families'] = vector3(0, 0, 0),
-    ['marabunta'] = vector3(1281.26, -1619.63, 27.23),
+    ['marabunta'] = vector3(1283.49, -1605.55, 25.98),
     ['rooks'] = vector3(22.93, 6472.1, 31.9),
     ['empireelite'] = vector3(-775.75, 800.26, 215.16),
 }
